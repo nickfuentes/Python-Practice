5510a702c1a200fece35f3cb541665181d28e85e
Nickolas Fuentes <>
2019-06-25 14:27:53 -0500

    Added the round function to make the tip total stop after two decimal places.

--- tip-calculator.py
+++ tip-calculator.py
@@ -1,6 +1,6 @@
 total_amount = input("Enter total amount: ")
 tip_percentage = input("Enter the tip percentage amount: ")
 
-result = float(total_amount) * (float(tip_percentage) / 100)
+result = round(float(total_amount) * (float(tip_percentage) / 100), 2)
 
 print(f"The total tip for the bill is ${result}!")
