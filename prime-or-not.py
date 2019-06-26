dfabd39220a48bff8c9672cf6239bc2d9cfae076
Nickolas Fuentes <>
2019-06-26 14:12:35 -0500

    Created an app that asks a user for a number and checks to see if it is prime or not.

---
+++ prime-or-not.py
@@ -0,0 +1,6 @@
+user_number = int(input("Enter a number to check if is is prime or not: "))
+
+if user_number % 2 == 0:
+    print(f"Your number {user_number} is Prime!")
+else:
+    print(f"Your number {user_number} is Not Prime!")
