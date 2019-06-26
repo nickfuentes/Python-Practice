c4d4088e62c735d3b5788fec865e4fef0f552c49
Nickolas Fuentes <>
2019-06-26 14:54:18 -0500

    Corrected a even number solution to make it check if the number is prime or not that the user has inputed.

--- prime-or-not.py
+++ prime-or-not.py
@@ -1,6 +1,12 @@
 user_number = int(input("Enter a number to check if is is prime or not: "))
 
-if user_number % 2 == 0:
-    print(f"Your number {user_number} is Prime!")
+if user_number > 1:
+    for index in range(2, user_number):
+        if (user_number % index) == 0:
+            print(f"Your number {user_number} is not a prime number")
+            break
+    else:
+        print(f"Your number {user_number} is a prime number")
+
 else:
-    print(f"Your number {user_number} is Not Prime!")
+    print(f"Your number {user_number} is not a prime number")
