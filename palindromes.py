39ff35971e109d36df796e669fd620299605da90
Full Name <nickfuentes24@gmail.com>
2019-06-30 14:11:30 -0500

    created a function that asks for a user to enter a word and check if its a palindrome.

--- palindromes.py
+++ palindromes.py
@@ -1,16 +1,30 @@
-user_word = input("Enter word to see if it is a Palindrome: ")
+
+"""user_word = input("Enter word to see if it is a Palindrome: ")
 
 reversed_word = ""
 
 for index in range(len(user_word) - 1, -1, -1):
-
+    # reversed_word
     reversed_word = reversed_word + user_word[index]
 
 if reversed_word == user_word:
     print(f"Your word {user_word} is palidrome")
 else:
-    print(f"Your word {user_word} is not a palidrome")
+    print(f"Your word {user_word} is not a palidrome")"""
 
-# Take einput
-# Reverse input
-# Compare ther reverse input with the actual input
+
+def palindrome():
+
+    word = input("Enter a word: ")
+    reversed_word = ""
+
+    for index in range(len(word) - 1, -1, -1):
+        reversed_word = reversed_word + word[index]
+
+    if word == reversed_word:
+        print("Your word is a Palindrome.")
+    else:
+        print("Your word is not a Palindrome.")
+
+
+palindrome()
