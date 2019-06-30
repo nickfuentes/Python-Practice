ba285812380812528341e14e7d48e9f0da9df15a
Full Name <nickfuentes24@gmail.com>
2019-06-30 12:38:19 -0500

    Using boolean to see if its prime or not

--- prime-or-not.py
+++ prime-or-not.py
@@ -1,4 +1,4 @@
-user_number = int(input("Enter a number to check if is is prime or not: "))
+"""user_number = int(input("Enter a number to check if is is prime or not: "))
 
 if user_number > 1:
     for index in range(2, user_number):
@@ -9,4 +9,20 @@ if user_number > 1:
         print(f"Your number {user_number} is a prime number")
 
 else:
-    print(f"Your number {user_number} is not a prime number")
+    print(f"Your number {user_number} is not a prime number")"""
+
+# prime numbers
+
+# take input from the user
+number = int(input("Enter a number: "))
+is_prime = True
+
+for index in range(2, number):
+    if number % index == 0:
+        is_prime = False
+        break
+
+if is_prime:
+    print("PRIME")
+else:
+    print("Not Prime")
