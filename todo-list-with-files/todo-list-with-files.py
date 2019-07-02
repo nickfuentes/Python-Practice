d5807ef93fe0c5f17e66a0d59aa18e6d29b76536
J. Fitzgerald <48025555+yourfriendfitz@users.noreply.github.com>
2019-07-02 17:37:33 -0500

    Update todo-list-with-files.py
    refactored how the JSON data was being accessed with get_json
    took the JSON access out to the end of the while =! q loop

--- todo-list-with-files/todo-list-with-files.py
+++ todo-list-with-files/todo-list-with-files.py
@@ -1,6 +1,11 @@
 import json
 todos = []
 
+def get_json(file_name, array):
+    with open(file_name, "w") as file_object:
+        json.dump(array, file_object)
+
+
 
 def print_menu():
     print("----- Todo App Menu -----")
@@ -13,9 +18,6 @@ def print_menu():
 
 user_input = ""
 
-with open("todo-list-with-files.json") as file_object:
-    json_data = json.load(file_object)
-todos.append(json_data)
 
 while user_input != "q":
     # need to add json data into an empty array when app starts
@@ -33,9 +35,6 @@ while user_input != "q":
         todos.append(todo)
         print("***** You have added a Todo! *****")
         print(todo)
-        with open("todo-list-with-files.json", "w") as file_object:
-            json.dump(todos, file_object)
-
     elif user_input == "2":
         print("***** Here is your Todo List! *****")
         for index in range(0, len(todos), 1):
@@ -43,9 +42,8 @@ while user_input != "q":
         delete_todo = int(
             input("Enter the index of the task you want to delete: "))
         del todos[delete_todo]
-        with open("todo-list-with-files.json", "w") as file_object:
-            json.dump(todos, file_object)
     elif user_input == "3":
         print("***** Here is your Todo List! *****")
         for index in range(0, len(todos), 1):
             print(index, todos[index])
+    get_json("todo-list-with-files.json", todos)
