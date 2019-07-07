f15ae73e6a5bfbcb165e1af65c97e506e940766f
Full Name <nickfuentes24@gmail.com>
2019-07-07 14:02:44 -0500

    Created a grocery list app that makes diffrent list and items to the list.

--- my-grocery-app/main.py
+++ my-grocery-app/main.py
@@ -1,25 +1,21 @@
+# pulling classes from other files and making it avaiable in main.py
 from shopping_list import ShoppingList
 from grocery_item import GroceryItem
 
-shopping_lists = []
-
-shopping_lists.append(shopping_list)
-shopping_lists.append(shopping_list2)
-shopping_list = ShoppingList("HEB", "1200 Richmond")
-shopping_list2 = ShoppingList("Walmart", "345 Harvin")
-
 
 def print_menu():
-    print("----- Todo App Menu -----")
-    print("Press (1) Create List")
-    print("Press (2) Add Items To List")
-    print("Press (3) View All Lists")
-    print("Press (q) To Quit")
-    print("-------------------------")
+    print("--------------------------------")
+    print("******* Grocery App Menu *******")
+    print("--------------------------------")
+    print("Press (1) Create Grocery List")
+    print("Press (2) Add Grocery Items")
+    print("Press (3) View Shopping List")
+    print("Press (q) to quit")
+    print("--------------------------------")
 
 
-print_menu()
-
+# list of shopping lists
+shopping_lists = []
 user_input = ""
 
 while user_input != "q":
@@ -27,5 +23,55 @@ while user_input != "q":
     user_input = input("What would you like to do? ")
 
     if user_input == "1":
-        title = input("Please enter store name: ")
-        address = input("Please enter store address: ")
+        store_name = input("Enter a store name: ")
+        store_address = input("Enter a store address: ")
+        shopping_list = ShoppingList(store_name, store_address)
+        shopping_lists.append(shopping_list)
+        while user_input != "n":
+            item_name = input("Enter item name: ")
+            item_price = input("Enter item price: ")
+            item_quantity = input("Enter item quanity: ")
+            grocery_item = GroceryItem(item_name, item_price, item_quantity)
+            shopping_list.grocery_items.append(grocery_item)
+            user_input = input(
+                "Would you like to add another item? yes = [y] / no = [n] ")
+        for shopping_list in shopping_lists:
+            print("--------------------------------")
+            print("***** Grocery List Created *****")
+            print("--------------------------------")
+            print(
+                f"{shopping_list.name} {shopping_list.address}")
+        for item in shopping_list.grocery_items:
+            print(f"Item: {item.name}")
+            print(f"Price: {item.price}")
+            print(f"Quantity: {item.quantity}")
+    elif user_input == "2":
+        print("--------------------------------")
+        print("****** Your Shopping Lists *****")
+        print("--------------------------------")
+        for list in shopping_lists:
+            print(f"Store: {list.name}")
+            print(f"Address: {list.address}")
+            print("Items:")
+            for item in list.grocery_items:
+                print(f"{item.name} Price: {item.price} Quantity: {item.quantity}")
+            print("--------------------------------")
+        while user_input != "n":
+            item_name = input("Enter item name: ")
+            item_price = input("Enter item price: ")
+            item_quantity = input("Enter item quanity: ")
+            grocery_item = GroceryItem(item_name, item_price, item_quantity)
+            shopping_list.grocery_items.append(grocery_item)
+            user_input = input(
+                "Would you like to add another item? yes = [y] / no = [n] ")
+    elif user_input == "3":
+        print("--------------------------------")
+        print("****** Your Shopping Lists *****")
+        print("--------------------------------")
+        for list in shopping_lists:
+            print(f"Store: {list.name}")
+            print(f"Address: {list.address}")
+            print("Items:")
+            for item in list.grocery_items:
+                print(f"{item.name} Price: {item.price} Quantity: {item.quantity}")
+            print("--------------------------------")
--- my-grocery-app/shopping_list.py
+++ my-grocery-app/shopping_list.py
@@ -1,5 +1,5 @@
 class ShoppingList:
     def __init__(self, name, address):
         self.name = name
-        self.address == address
+        self.address = address
         self.grocery_items = []
